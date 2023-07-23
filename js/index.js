ae0299220ccb9dc24f3b0b4d1accfd38bad133b7
lyoma <lyomaikt@gmail.com>
2023-07-24 01:25:45 +0300

    first-Js

---
+++ js/index.js
@@ -0,0 +1,30 @@
+//alert("я JavaScript!");
+
+// неправильные переменные
+/*
+    let 1number = 1;
+    let num-1 = 1;
+*/
+
+// правильные переменные
+
+/*
+    let numberOne = 5;
+    let numberTwo = 2;
+    const numberTrird = 3;
+*/
+
+let numberOne = 9,
+    numberTwo = 4;
+const numberTrird = 3;
+
+
+console.log("numberOne:", numberOne, "\nnumberTwo:", numberTwo);
+
+// код выводит простые операции( * / + - ** ) в консоль
+console.log("+", numberOne + numberTwo);
+console.log("-", numberOne - numberTwo);
+console.log("*", numberOne * numberTwo);
+console.log("/", numberOne / numberTwo);
+console.log("**", numberOne ** numberTwo);
+console.log("sqr", numberOne**0.5, numberTwo**0.5);
